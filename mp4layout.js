223eca8399e28e7c663eb10e9c3df67732d8c918
Norbert Waschbuesch <obstsalad@users.noreply.github.com>
2017-04-22 22:00:35 -0700

    Adding mp4layout.js file

---
+++ mp4layout.js
@@ -0,0 +1,13 @@
+﻿/**
+ * mp4layout
+ * ver 1.0   2017/04/22   Initial draft
+ *
+ */
+"use strict";
+
+var mp4layoutFct = function(mp4uri, cb) {  
+    console.log("mp4layout called");
+}; // end of mp4layoutFct() definition
+
+
+
